391f51e58c385e50e8d2a2c6da8f2f295bbb82f0
Charan <charankulal0241@gmail.com>
2023-12-03 17:18:43 +0530

    Given smoothing effect for volume control.

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class HandDetector():
     def __init__(self,mode=False,maxHands=2,detectionCon=1,trackCon=1):
@@ -59,4 +60,18 @@ class HandDetector():
             else:
                 fingers.append(0)
         return fingers
-
+    
+    def findDistance(self,p1,p2,img,draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
+        
+        if draw:
+            cv2.circle(img, (x1, y1), 8, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 8, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
+            cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
+            
+        length = math.hypot(x2-x1, y2-y1)
+        
+        return length,img,[x1,y1,x2,y2,cx,cy]
