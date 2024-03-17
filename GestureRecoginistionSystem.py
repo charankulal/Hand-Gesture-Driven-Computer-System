9a4b71d1dade1f00e6feb117cee9d21c7846c4a8
Charan <charankulal0241@gmail.com>
2024-03-17 11:42:12 +0530

    Added screen brightness control using left hand.

--- GestureRecoginistionSystem.py
+++ GestureRecoginistionSystem.py
@@ -24,8 +24,8 @@ clocX, clocY = 0, 0
 
 detector = htm.HandDetector(maxHands=1)
 
-# `wScreen, hScreen = pyautogui.size()` retrieves the screen resolution width and height using the `pyautogui.size()` function 
-# and assigns them to the variables `wScreen` and `hScreen` respectively. This information is useful for mapping 
+# `wScreen, hScreen = pyautogui.size()` retrieves the screen resolution width and height using the `pyautogui.size()` function
+# and assigns them to the variables `wScreen` and `hScreen` respectively. This information is useful for mapping
 # the hand movements on the camera feed to the corresponding positions on the screen.
 
 # declarations related to mouse control functionalities
@@ -53,16 +53,16 @@ while True:
     # lmList, bbox = detector.findPosition(img)
     lmList, boundingBox = detector.findPosition(img, handNo=0, draw=True)
 
-    if which_hand=="Right" :
+    if which_hand == "Right":
         if len(lmList) != 0:
             [x1, y1] = lmList[8][1:]
             [x2, y2] = lmList[12][1:]
             fingers = detector.fingersUp()
             cv2.rectangle(img, (frameR, frameR),
-                        (wCam-frameR, hCam-frameR), (255, 0, 255), 2)
+                          (wCam-frameR, hCam-frameR), (255, 0, 255), 2)
 
             # For hovering or pointer movement
-            if fingers[1] == 1 and fingers[2] == 1 and fingers[0]==0:
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0:
 
                 x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScreen))
                 y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScreen))
@@ -75,15 +75,14 @@ while True:
 
             # Left click functionality
 
-            if fingers[1] == 1 and fingers[2] == 1 and fingers[3]==1 and fingers[4]==0 :
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 0:
                 pyautogui.click()
-                
 
             # Right click functionality
-            if fingers[1] == 1 and fingers[2] == 0:
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0:
                 if fingers[4] == 1:
                     pyautogui.rightClick()
-                    
+
             area = (boundingBox[2]-boundingBox[0]) * \
                 (boundingBox[3]-boundingBox[1])//100
             # print(area)
@@ -100,12 +99,60 @@ while True:
                 vol = smoothness*round(vol/smoothness)
 
                 # Check fingers which are up
-                fingers=detector.fingersUp()
-            
+                fingers = detector.fingersUp()
 
                 # if pinky is down then set volume
                 if not fingers[4]:
                     volume.SetMasterVolumeLevelScalar(vol/100, None)
+    if which_hand == "Left":
+        if len(lmList) != 0:
+            [x1, y1] = lmList[8][1:]
+            [x2, y2] = lmList[12][1:]
+            fingers = detector.fingersUp()
+            cv2.rectangle(img, (frameR, frameR),
+                          (wCam-frameR, hCam-frameR), (255, 0, 255), 2)
+
+            # For hovering or pointer movement
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 1:
+
+                x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScreen))
+                y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScreen))
+
+                clocX = plocX+(x3-plocX)/smoothening
+                clocY = plocY+(y3-plocY)/smoothening
+
+                pyautogui.moveTo(clocX, clocY)
+                plocX, plocY = clocX, clocY
+
+            # Left click functionality
+
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 0:
+                pyautogui.click()
+
+            # Right click functionality
+            if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0:
+                if fingers[4] == 1:
+                    pyautogui.rightClick()
+
+            area = (boundingBox[2]-boundingBox[0]) * \
+                (boundingBox[3]-boundingBox[1])//100
+            # print(area)
+            if not fingers[2]:
+                if 100 < area < 900:
+
+                    # Find the distance btwn index and thumb
+                    length, img, lineInfo = detector.findDistance(4, 8, img=img)
+
+                    # Converting length to volume
+                    brightness = np.interp(length, [10, 150], [0, 100])
+
+                    # Reduce resolution to make smoother.
+                    smoothness = 5
+                    brightness = smoothness*round(brightness/smoothness)
+
+                    # if pinky is down then set volume
+                    if not fingers[4]:
+                        sbc.set_brightness(brightness)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
