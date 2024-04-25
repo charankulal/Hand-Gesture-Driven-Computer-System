3a7775442688d9a46ce40485af1168dbe1dcfb91
Charan <charankulal0241@gmail.com>
2024-04-25 10:27:45 +0530

    Updated the project files to latest versions.

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -6,6 +6,7 @@ import cv2
 import threading
 from deepface import DeepFace
 
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -18,14 +19,15 @@ reference_img = cv2.imread("sample1.jpg")
 
 def check_face(frame):
     global face_match
+    
     try:
         if DeepFace.verify(frame, reference_img.copy())['verified']:
-            face_match=True
+            face_match = True
         else:
-            face_match=False
-            
-    except ValueError:
-        face_match=False
+            face_match = False
+    except ValueError as e:
+        print("Error:", e)
+        face_match = False
 
 
 while True:
@@ -34,10 +36,10 @@ while True:
     if ret:
         if counter % 30 == 0:
             try:
-                threading.Thread(target=check_face,
-                                 args=(frame.copy(),)).start()
-            except ValueError:
-                pass
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError as e:
+                print("Error:", e)
+                face_match = False
 
         counter += 1
 
